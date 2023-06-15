cb9836dcb196b48926f5fed282d5568378391509
Navyasriveerisetty <133081344+Navyasriveerisetty@users.noreply.github.com>
2023-06-15 21:04:36 +1000

    Add files via upload

---
+++ StudentRegistrationn/src/com/softra/model/Student.java
@@ -0,0 +1,49 @@
+package com.softra.model;
+
+public class Student {
+    private String name;
+    private int age;
+    private String mobileno;
+    private String address;
+
+    
+    public Student(String name, int age, String mobileNumber, String address) {
+        this.name = name;
+        this.age = age;
+        this.mobileno = mobileno;
+        this.address = address;
+    }
+
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getMobileno() {
+        return mobileno;
+    }
+
+    public void setMobileno(String mobileno) {
+        this.mobileno = mobileno;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
